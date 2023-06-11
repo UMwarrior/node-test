a6fd5252207bd8d7e2c6d002a94fb7ccab39c52b
UMwarrior <82050103+UMwarrior@users.noreply.github.com>
2023-06-11 18:54:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
+  
+  console.log("Server running on port 80");
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Heleo Electronics');
-  console.log("Server running on port 80");
+  
 }).listen(80);
