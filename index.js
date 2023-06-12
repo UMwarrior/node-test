bed0e168035d5696af4603238610d4bfaef47e6e
UMwarrior <82050103+UMwarrior@users.noreply.github.com>
2023-06-12 13:55:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-var http = require('http');
+// var http = require('http');
 
-http.createServer(function (req, res) {
+// http.createServer(function (req, res) {
   
-  console.log("Server running on port 80");
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Heleo Electronics');
+//   console.log("Server running on port 80");
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Hello Heleo Electronics');
   
-}).listen(80);
+// }).listen(80);
+
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+  key: fs.readFileSync('6497065d390a8d58.pem', 'utf8'),
+  cert: fs.readFileSync('6497065d390a8d58.crt', 'utf8'),
+};
+
+const server = https.createServer(options, (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello Heleo Electronics');
+});
+
+server.listen(443, () => {
+  console.log('HTTPS server listening on port 443');
+});
